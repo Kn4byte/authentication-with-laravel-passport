86151afbfc76b052f2a95dc79c0439331f63aace
Kn4byte <31645443+Kn4byte@users.noreply.github.com>
2020-07-28 22:21:58 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/test', ['middleware' => 'auth'], function () {
+Route::get('/test', function () {
     return view('test');
-});
+})->middleware('auth');
